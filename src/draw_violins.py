b2bbf45b1054d13b3b7eee929de13aebcbd4b182
Vered Shwartz <vered1986@gmail.com>
2021-11-06 19:23:02 -0700

    ...

--- src/draw_violins.py
+++ src/draw_violins.py
@@ -1,10 +1,14 @@
+import os
 import json
+import logging
 import argparse
 import pandas as pd
 import seaborn as sns
 
-sns.set_theme(style="whitegrid")
-sns.set(rc={'figure.figsize': (16, 16)}, font_scale=2)
+sns.set(rc={'figure.figsize': (6, 6)}, style="whitegrid")
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def main():
@@ -17,32 +21,34 @@ def main():
 
     start_and_end_times, grounding = [], []
 
-    for type in ["numerals", "cardinals", "numerals_cardinals", "regex"]:
-        for model in ["extractive", "lm_based"]:
-            with open(f"output/{model}/{type}/{args.lang}_24.json") as f_in:
-                grd = json.load(f_in)
-                start_end = {exp: (grd[exp]["start"], grd[exp]["end"]) for exp in grd.keys()}
-                grd = {exp: {int(h): score for h, score in per_exp.items() if h not in {"start", "end"}}
-                       for exp, per_exp in grd.items()}
+    for model in ["extractive", "lm_based"]:
+        file = f"output/{model}/{args.lang}_24.json"
+        if not os.path.exists(file): continue
 
-                # Night: add 24 to the hours < 12
+        with open(file) as f_in:
+            grd = json.load(f_in)
+            start_end = {exp: (grd[exp]["start"], grd[exp]["end"]) for exp in grd.keys()}
+            grd = {exp: {int(h): score for h, score in per_exp.items() if h not in {"start", "end"}}
+                   for exp, per_exp in grd.items()}
+
+            # Night: add 24 to the hours < 12
+            if "night" in start_end:
                 if start_end["night"][1] < 12:
                     start_end["night"] = (start_end["night"][0], start_end["night"][1] + 24)
 
                 grd["night"] = {h + 24 if h < 12 else h: vals for h, vals in grd["night"].items()}
 
-                # Convert the probability into frequency distribution
-                grd = {exp: {h: int(score * 100) for h, score in per_exp.items()} for exp, per_exp in grd.items()}
+            # Convert the probability into frequency distribution
+            grd = {exp: {h: int(score * 100) for h, score in per_exp.items()} for exp, per_exp in grd.items()}
 
-                start_and_end_times.append(start_end)
-            grounding.append(grd)
+            start_and_end_times.append(start_end)
+        grounding.append(grd)
 
+    if len(grounding) == 2:
         labels = [l for l in labels if l in grounding[0]]
         ax = draw_violin(grounding, labels, start_and_end_times)
         fig = ax.get_figure()
-        fig.savefig(f"output/plots/{type}/{args.lang}.png")
-
-    fig.show()
+        fig.savefig(f"output/plots/{args.lang}.png")
 
 
 def draw_violin(grounding, labels, start_end_times, start_end_in_xaxis=False):
@@ -63,38 +69,30 @@ def draw_violin(grounding, labels, start_end_times, start_end_in_xaxis=False):
 
     df = pd.DataFrame.from_dict(d)
     ax = sns.violinplot(x="Expression", y="Time", hue="Model", data=df, order=labels, palette="muted")
-    sns.move_legend(ax, "lower center", bbox_to_anchor=(.5, 1), ncol=3, title=None, frameon=False)
+    sns.move_legend(ax, "best", ncol=2, title=None)
+    ax.set(xlabel=None, ylabel=None)
 
     # Set the times
-    times = range(1, max(32, max(d["Time"])))
-    ax.set_yticks(times)
+    times = range(1, max(37, max(d["Time"])))
+    ax.set_yticks(range(1, max(37, max(d["Time"])), 2))
+    ax.set_ylim([-1, len(times) + 1])
     num_to_time = {12: "12 pm", 24: "12 am"}
-    num_to_time.update({i: f"{i} am" for i in range(1, 12)})
-    num_to_time.update({i: f"{i-12} pm" for i in range(13, 24)})
-    num_to_time.update({i: f"{i - 24} am" for i in range(25, 36)})
+    num_to_time.update({i: f"{i} am" for i in range(1, 13)})
+    num_to_time.update({i: f"{i - 12} pm" for i in range(13, 25)})
+    num_to_time.update({i: f"{i - 24} am" for i in range(25, 37)})
     ax.set_yticklabels([num_to_time[num] for num in ax.get_yticks()])
 
     # Annotate start and end times
-    if start_end_in_xaxis:
-        ext_start = {exp: num_to_time[start_end_times[0][exp][0]] for exp in labels}
-        ext_end = {exp: num_to_time[start_end_times[0][exp][1]] for exp in labels}
-        lm_start = {exp: num_to_time[start_end_times[1][exp][0]] for exp in labels}
-        lm_end = {exp: num_to_time[start_end_times[1][exp][1]] for exp in labels}
-        ax.set_xticklabels([f"{exp}:\nExt:{ext_start[exp]}-{ext_end[exp]}\nLM:{lm_start[exp]}-{lm_end[exp]}"
-                            for exp in labels])
-    else:
-        width = 0.42
-        base_x = -0.2
-        linestyles = ["dashed", "solid"]
-        for model_i, curr_start_end_times in enumerate(start_end_times):
-            for exp_i, exp in enumerate(labels):
-                start, end = curr_start_end_times[exp]
-                ax.plot([base_x + width * (2.38 * exp_i + model_i) - 0.16,
-                         base_x + width * (2.38 * exp_i + model_i) + 0.16],
-                        [start, start], color="black", linewidth=3, linestyle=linestyles[model_i])
-                ax.plot([base_x + width * (2.38 * exp_i + model_i) - 0.16,
-                         base_x + width * (2.38 * exp_i + model_i) + 0.16],
-                        [end, end], color="black", linewidth=3, linestyle=linestyles[model_i])
+    width = 0.42
+    base_x = -0.2
+    colors = ["royalblue", "orange"]
+    for model_i, curr_start_end_times in enumerate(start_end_times):
+        for exp_i, exp in enumerate(labels):
+            start, end = curr_start_end_times[exp]
+            ax.scatter(base_x + width * (2.38 * exp_i + model_i) - 0.16, start,
+                       color=colors[model_i], edgecolor="black", marker="^")
+            ax.scatter(base_x + width * (2.38 * exp_i + model_i) - 0.16, end,
+                       color=colors[model_i], edgecolor="black", marker="v")
 
     return ax
 
